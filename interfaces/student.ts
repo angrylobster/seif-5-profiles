2110a015be90d24e27b4ea875ddc0561cdf0e458
Min Shan <liewminshan@gmail.com>
2021-04-24 10:00:22 +0800

    fix type issue

--- interfaces/student.ts
+++
@@ -1,8 +0,0 @@
-import { NameDetails } from './auth';
-import { HomeworkRecords } from './homework';
-
-export interface StudentProfile {
-    email: string;
-    name: NameDetails;
-    homework: HomeworkRecords;
-}
